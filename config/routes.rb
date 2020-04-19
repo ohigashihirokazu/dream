29c83fa532e22a1dc09e144a4858b6ce30ae7d43
ohigashihirokazu <hirokazudesu.1222@gmail.com>
2020-04-19 11:48:34 +0000

    mathing

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
-  resources :dreams
 
   get 'users/matching', to: 'users#matching'
 
-  resources :users, only: [:show, :index, :edit, :update, :destroy]
-  
+  resources :users, only: [:show, :index, :edit, :update, :destroy] do
+   member do
+     get :following, :followers
+   end
+   collection do
+    get :matching
+   end
+ end
+
   resources :dreams, only: [:new, :create, :index, :show] do
     resource :favorites, only: [:create, :destroy]
     resource :dream_comments, only: [:create, :destroy]
@@ -15,14 +21,4 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
 
-  constraints(id: /\d+/) do
-   resources :users do
-    member do
-     get :following, :followers
-   end
-   collection do
-    get :matching
-    end
-   end
-  end
 end
